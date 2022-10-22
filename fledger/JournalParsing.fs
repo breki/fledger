b4be35f7143a94182a4e82e0547cc0b041061b81
Igor Brejc <igor.brejc@gmail.com>
2022-10-22 23:47:40 +0200

    Extracted ParsingBasics module.

--- fledger/JournalParsing.fs
+++ fledger/JournalParsing.fs
@@ -1,55 +1,13 @@
 ﻿module fledger.JournalParsing
 
-open Journal
-
 open System
-open System.Globalization
 
+open System.Globalization
 open FParsec
 
-let filterOutNone items =
-    items
-    |> (List.filter Option.isSome)
-    |> List.map Option.get
+open fledger.Journal
+open fledger.ParsingBasics
 
-let trimStringOptional (s: string option) =
-    match s with
-    | Some s ->
-        s.Trim()
-        |> function
-            | "" -> None
-            | trimmed -> Some trimmed
-    | None -> None
-
-let whitespace: Parser<string, unit> =
-    manyChars (pchar ' ' <|> pchar '\t')
-    <?> "whitespace"
-
-let whitespace1: Parser<string, unit> =
-    many1Chars (pchar ' ' <|> pchar '\t')
-    <?> "whitespace 1"
-
-let endOfLineWhitespace: Parser<unit, unit> =
-    whitespace >>. newline >>% ()
-    <?> "end of line whitespace"
-
-let pEmptyLine =
-    endOfLineWhitespace <?> "empty line"
-
-let pDatePartSeparator: Parser<char, unit> =
-    pchar '/' <|> pchar '-' <?> "date part separator"
-
-let pYear =
-    pint32 .>> pDatePartSeparator <?> "year"
-
-let pMonth =
-    pint32 .>> pDatePartSeparator <?> "month"
-
-let pDay = pint32 <?> "day"
-
-let pDate =
-    pipe3 pYear pMonth pDay (fun year month day -> DateTime(year, month, day))
-    <?> "date"
 
 // status character = "!" | "*"
 let pTxStatus: Parser<char, unit> =
---
+++ fledger/ParsingBasics.fs
@@ -0,0 +1,49 @@
+﻿module fledger.ParsingBasics
+
+open System
+
+open FParsec
+
+let filterOutNone items =
+    items
+    |> (List.filter Option.isSome)
+    |> List.map Option.get
+
+let trimStringOptional (s: string option) =
+    match s with
+    | Some s ->
+        s.Trim()
+        |> function
+            | "" -> None
+            | trimmed -> Some trimmed
+    | None -> None
+
+let whitespace: Parser<string, unit> =
+    manyChars (pchar ' ' <|> pchar '\t')
+    <?> "whitespace"
+
+let whitespace1: Parser<string, unit> =
+    many1Chars (pchar ' ' <|> pchar '\t')
+    <?> "whitespace 1"
+
+let endOfLineWhitespace: Parser<unit, unit> =
+    whitespace >>. newline >>% ()
+    <?> "end of line whitespace"
+
+let pEmptyLine =
+    endOfLineWhitespace <?> "empty line"
+
+let pDatePartSeparator: Parser<char, unit> =
+    pchar '/' <|> pchar '-' <?> "date part separator"
+
+let pYear =
+    pint32 .>> pDatePartSeparator <?> "year"
+
+let pMonth =
+    pint32 .>> pDatePartSeparator <?> "month"
+
+let pDay = pint32 <?> "day"
+
+let pDate =
+    pipe3 pYear pMonth pDay (fun year month day -> DateTime(year, month, day))
+    <?> "date"
