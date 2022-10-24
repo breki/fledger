f1c8ba854f718179b6efa69e502f55eb78163053
Igor Brejc <igor.brejc@gmail.com>
2022-10-25 01:08:33 +0200

    Added todos for missing directives.

--- fledger/ParsingJournal.fs
+++ fledger/ParsingJournal.fs
@@ -7,6 +7,10 @@ open fledger.ParsingBasics
 open fledger.ParsingDefaultCommodity
 open fledger.ParsingTransactions
 
+// todo 10: add support for "commodity EUR" directives
+// todo 40: add support for "P 2018-11-01 CAD 0.671 EUR" directives
+// todo 70: add support for account directives
+// todo 100: add support for account directives
 
 let pJournalItem<'T> : Parser<JournalItem option, 'T> =
     (pTx |>> Transaction |>> Some)
