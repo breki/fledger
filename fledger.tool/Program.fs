6d15fd081d5ea839879abdbd8ebda57db9f93459
Igor Brejc <igor.brejc@gmail.com>
2022-11-05 18:13:06 +0100

    Added parsing of the journal to the CLI command.

--- fledger.tool/Program.fs
+++ fledger.tool/Program.fs
@@ -1,2 +1,14 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+open FParsec
+open fledger.Parsing.ParsingBasics
+open fledger.Parsing.ParsingJournal
+
+let text =
+    File.ReadAllText(@"D:\ledger\igor.ledger")
+
+let result =
+    runParserOnString pJournal { Something = 0 } "test stream" text
+
+match result with
+| Success (journal, _, _) -> printfn "Success"
+| Failure (errorMsg, _, _) -> printfn $"PARSING ERROR: {errorMsg}"
