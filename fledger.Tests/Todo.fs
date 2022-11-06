6b006f3a16df82b8b2598038b7370c9d9a24148e
Igor Brejc <igor.brejc@gmail.com>
2022-11-06 12:49:54 +0100

    total-balance.html now has the chart creation split into subfunctions.

--- fledger.Tests/Todo.fs
+++ fledger.Tests/Todo.fs
@@ -1,4 +1,6 @@
 ﻿module fledger.Tests.Todo
 
-// todo 15: beautify the chart - axes etc.
-// todo 20: expose the command as subcommand of the main command
+// todo 15: exclude initial balances up to certain date
+// todo 16: y-axis should start at the min balance after that date
+// todo 20: beautify the chart: additional trendline?
+// todo 40: expose the command as subcommand of the main command
