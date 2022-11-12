f0e4c6144b5f9a95b39cdcf31656a9f107eb82e8
Igor Brejc <igor.brejc@gmail.com>
2022-11-12 19:36:18 +0100

    Updated todos.

--- fledger.Tests/Todo.fs
+++ fledger.Tests/Todo.fs
@@ -1,4 +1,5 @@
 ﻿module fledger.Tests.Todo
 
 // todo 30: monthly running average of income and expenses
-// todo 50: which else visualizations should be done?
+// todo 40: monthly running average for major income and expenses categories
+// todo 50: which other visualizations should be done?
