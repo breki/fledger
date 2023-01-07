864b33f48ee2f53c090d6c2f08e9c70a5340583f
Igor Brejc <igor.brejc@gmail.com>
2023-01-07 19:03:26 +0100

    First implementation of transactionsAreChronologicallyOrdered() function.

--- fledger.Tests/Todo.fs
+++ fledger.Tests/Todo.fs
@@ -1,5 +1,10 @@
 ﻿module fledger.Tests.Todo
 
+// todo 10: implement journal validator
+// todo 12: validator: transactions should be in chronological order
+// todo 13: validator: total amount of a transaction should be 0
+// todo 14: validator: the expected account totals should be validated
+
 // todo XX 20: monthly running average for major income and expenses categories
 // todo XX 60: beautify charts
 // todo XX 50: which other visualizations should be done?
---
+++ fledger.Tests/validating/validating chronological order.fs
@@ -0,0 +1,72 @@
+﻿module fledger.Tests.validating.validating_chronological_order
+
+open System
+open Xunit
+open Swensen.Unquote
+open fledger.Ledger
+open fledger.BasicTypes
+
+// type Ledger =
+//     { Accounts: Map<AccountRef, Account>
+//       Transactions: Transaction list
+//       MarketPrices: MarketPrices }
+
+// type Account = { Name: AccountRef }
+
+// type AccountRef =
+//     { FullName: AccountName
+//       NameParts: string[] }
+//
+//     static member Create(fullName: string) =
+//         let parts = fullName.Split(':')
+//
+//         { FullName = fullName
+//           NameParts = parts }
+
+// type Transaction =
+//     { Date: Date
+//       Status: TransactionStatus
+//       Description: string option
+//       Payee: string option
+//       Note: string option
+//       Comment: string option
+//       Postings: Posting list }
+
+
+let transactionsAreChronologicallyOrdered ledger =
+    ledger.Transactions
+    |> List.pairwise
+    |> List.forall (fun (t1, t2) -> t1.Date <= t2.Date)
+
+[<Fact>]
+let ``transactions should be in chronological order`` () =
+    // create ledger with single account and two transactions
+
+    let accountRef = AccountRef.Create "Assets:Checking"
+    let account = { Name = accountRef }
+
+    // todo 5: we need a tx builder
+    let tx1 =
+        { Date = DateTime(2023, 1, 7)
+          Status = TransactionStatus.Unmarked
+          Description = None
+          Payee = None
+          Note = None
+          Comment = None
+          Postings = [] }
+
+    let tx2 =
+        { Date = DateTime(2023, 1, 6)
+          Status = TransactionStatus.Unmarked
+          Description = None
+          Payee = None
+          Note = None
+          Comment = None
+          Postings = [] }
+
+    let ledger =
+        { Accounts = [ accountRef, account ] |> Map.ofList
+          Transactions = [ tx1; tx2 ]
+          MarketPrices = { Prices = Map.empty } }
+
+    test <@ transactionsAreChronologicallyOrdered ledger = false @>
